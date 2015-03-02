adbdb1c11ca5cb4986a5344481c501c538362e5d
Dyson Simmons <dysonsimmons@gmail.com>
2015-03-02 16:06:46 +1100

    Initial commit.

---
+++ js/library.js
@@ -0,0 +1,43 @@
+/*! library.js v0.0.1 | MIT License | dysonsimmons.com/library.js */
+
+(function (window, undefined) {
+
+  var document = window.document,
+      L = function(elements) {
+        return new Library(elements);
+      },
+      Library = function(elements) {
+        this.length = elements.length;
+        if (elements.length === undefined) { // only have one element
+          this.length = 1;
+          this[0] = elements;
+        } else { // we have a collection of elements
+          for (var i = 0; i < this.length; i++) {
+            this[i] = elements[i];
+          }
+        }
+        return this;
+      };
+
+  L.fn = Library.prototype = {
+    ready: function(fn) {
+      if (document.readyState != 'loading'){
+        fn();
+      } else {
+        document.addEventListener('DOMContentLoaded', fn);
+      }
+    },
+    each: function(fn) {
+      for (var i = 0; i < this.length; i++) {
+        fn.call(this[i], i, this[i]);
+      }
+      return this;
+    }
+  };
+
+  if(! window.L) {
+    window.L = L;
+  }
+
+})(window);
+
